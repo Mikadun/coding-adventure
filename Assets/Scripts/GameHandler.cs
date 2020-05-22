f3f115add991d4add53416a4348bd1145269863f
JIeHb <gribak98@list.ru>
2020-05-22 19:53:55 +1000

    Ball Game

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public Ball ball;
+    public Paddle paddle;
+
+    public static Vector2 bottomLeft;
+    public static Vector2 topRight;
+    void Start()
+    {
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        Instantiate(ball);
+        Paddle paddle1 = Instantiate(paddle) as Paddle;
+        Paddle paddle2 = Instantiate(paddle) as Paddle;
+        paddle1.Init(true);
+        paddle2.Init(false);
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    public float speed = 10f;
+
+    private float height;
+    private bool isRight;
+    private string input = "Horizontal";
+
+    void Start()
+    {
+        height = transform.localScale.y;
+    }
+
+    public void Init(bool isRightPaddle)
+    {
+        isRight = isRightPaddle;
+        Vector2 pos = Vector2.zero;
+
+        if (isRightPaddle)
+        {
+            pos = new Vector2(GameHandler.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x;
+        }
+        else
+        {
+            pos = new Vector2(GameHandler.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x;
+
+        }
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        if (transform.position.y < GameHandler.bottomLeft.y + height / 2 && move < 0)
+        {
+            move = 0;
+        }
+        if (transform.position.y > GameHandler.topRight.y - height / 2 & move > 0)
+        {
+            move = 0;
+        }
+        transform.Translate(move * Vector2.up);
+    }
+}
