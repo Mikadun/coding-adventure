ee9c621f3e00b43bffd093b2ca846397348e52c0
Joker1379 <jokerviktor13@gmail.com>
2020-05-28 18:33:01 +1000

    Add options

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public void OnValueChanged(string option)
+    {
+        if(option == "VolumeControll")
+        {
+            //GetComponent<AudioSource>().volume = GameObject.Find("VolumeControll").GetComponent<Slider>().value;
+        }
+    }
+}
